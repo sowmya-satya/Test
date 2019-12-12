d302b90a7762010ae2c6b9f13699d1e7fb5b687a
M1056314 <M1056314@G1C2ML35469.mindtree.com>
2019-12-12 10:15:22 +0530

    commited in app2

--- TestProject/src/App2.java
+++ TestProject/src/App2.java
@@ -3,5 +3,7 @@ public class App2 {
 
 	public static void main(String args[]) {
 		System.out.println("This is second app");
+		System.out.println("This is to branch 1");
+		System.out.println("Third line...");
 	}
 }
