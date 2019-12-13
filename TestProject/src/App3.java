bcfaf9c0514e9d577ace092f2781d6b266c8b3b4
sowmya-satya <58682121+sowmya-satya@users.noreply.github.com>
2019-12-13 08:53:34 +0530

    Update App3.java

--- TestProject/src/App3.java
+++ TestProject/src/App3.java
@@ -3,5 +3,8 @@ public class App3 {
 public static void main(String args[])
 {
 	System.out.println("This is my third app");
+	System.out.println("This is my third app");
+	System.out.println("This is my third app");
+	System.out.println("This is my third app");
 }
 }
